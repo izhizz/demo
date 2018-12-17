1bf43d1c5be85a3dc0f2afdeb3bf6bdd26924c61
fengzhi <1336143962@qq.com>
2018-12-17 14:46:34 +0800

    整理Lucene ，简单使用Lucene

--- src/main/java/lucene/utils/SimpleLucene.java
+++ src/main/java/lucene/utils/SimpleLucene.java
@@ -1,101 +1,23 @@
-package lucene.service.impl;
+package lucene.utils;
 
-
-import lucene.service.LuceneService;
-import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.*;
-
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.store.*;
-import org.apache.lucene.util.Version;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class LuceneServiceImpl implements LuceneService {
-    @Autowired
-
-//    @Override
-//    public void createIndex() throws IOException {
-//        //创建索引
-//        Path path = Paths.get("E:\\programme\\test");
-//        //指定索引库的存放位置Directory对象
-//        Directory directory = FSDirectory.open(path);
-//        /*
-//        索引库还可以存放到内存中
-//        Directory directory = new RAMDirectory();
-//        指定一个标准分析器，对文档内容进行分析
-//        */
-//
-//        Analyzer analyzer = new StandardAnalyzer();
-//
-//        //创建indexwriterCofig对象
-//        //第一个参数： Lucene的版本信息，可以选择对应的lucene版本也可以使用LATEST
-//        //第二根参数：分析器对象
-//        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-//
-//        //创建一个indexwriter对象
-//        IndexWriter indexWriter = new IndexWriter(directory, config);
-//
-//        //原始文档的路径
-//        File file = new File("E:\\programme\\searchsource.txt");
-//        File[] fileList = file.listFiles();
-//        for (File file2 : fileList) {
-//            //创建document对象
-//            Document document = new Document();
-//
-//            //创建field对象，将field添加到document对象中
-//
-//            //文件名称
-//            String fileName = file2.getName();
-//            //创建文件名域
-//            //第一个参数：域的名称
-//            //第二个参数：域的内容
-//            //第三个参数：是否存储
-//            Field fileNameField = new TextField("fileName", fileName, Field.Store.YES);
-//
-//            //文件的大小
-////            long fileSize = FileUtils.sizeOf(file2);
-//            //文件大小域
-////            Field fileSizeField = new LongRange("fileSize", fileSize, Field.Store.YES);
-//
-//            //文件路径
-//            String filePath = file2.getPath();
-//            //文件路径域（不分析、不索引、只存储）
-//            Field filePathField = new StoredField("filePath", filePath);
-//
-//            //文件内容
-//            String fileContent = FileUtils.readFileToString(file2);
-//            //String fileContent = FileUtils.readFileToString(file2, "utf-8");
-//            //文件内容域
-//            Field fileContentField = new TextField("fileContent", fileContent, Field.Store.YES);
-//
-//            document.add(fileNameField);
-////            document.add(fileSizeField);
-//            document.add(filePathField);
-//            document.add(fileContentField);
-//            //使用indexwriter对象将document对象写入索引库，此过程进行索引创建。并将索引和document对象写入索引库。
-//            indexWriter.addDocument(document);
-//        }
-//        //关闭IndexWriter对象。
-//        indexWriter.close();
-//    }
-
-//    @Override
+public class SimpleLucene {
     public static Directory createSimpleIndex(Analyzer analyzer, List<String> products) throws IOException {
         Directory index = new RAMDirectory();
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
